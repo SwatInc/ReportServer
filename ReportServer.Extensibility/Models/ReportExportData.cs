e244cf37962a00f7336d1f29872916431e5e0975
ibrahimhuycn <ibrahim.hucyn@live.com>
2022-01-17 19:47:25 +0500

    Added more fields required for report exporting

---
+++ ReportServer.Extensibility/Models/ReportExportData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportServer.Extensibility.Models
+{
+    public class ReportExportDataModel
+    {
+        public string SampledSite { get; set; }
+        public string Nidpp { get; set; }
+        public string EpisodeNumber { get; set; }
+        public string PatientName { get; set; }
+    }
+}
